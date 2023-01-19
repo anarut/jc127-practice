9676115a66ba2be253079e8ccd168828a061de8f
Andrey Narut <andrey.narut.90@gmail.com>
2023-01-19 19:13:56 +0300

    lesson 10

---
+++ src/lesson10/Main.java
@@ -0,0 +1,31 @@
+package lesson10;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    class A { }
+
+    class B extends A { }
+
+    class C extends A { }
+
+    class Bouquet {
+
+        A[] flowers;
+
+        public Bouquet(int size) {
+            flowers = new A[size];
+        }
+
+        //add()
+
+        int price() {
+            // flowers
+            return 0;
+        }
+
+    }
+}
